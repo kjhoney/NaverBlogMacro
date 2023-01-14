bae0a6118daf9834507a26cb88aa61c7125847ee
kjhoney <kjhkjh3922@naver.com>
2023-01-14 17:08:14 +0900

    feat: 이웃신청 내용 추가

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const puppeteer = require('puppeteer');
 
-// 여기에 id, pw, 블로그 글주소 복붙
+// 여기에 id, pw, 블로그 글주소, 신청내용 복붙
 const naver_id = '';
 const naver_pw = '';
 const post_url = 'https://blog.naver.com/kiti817/222983317744';
+const requestContent = `안녕하세요 :)
+호랑이띠 아기와 함께하는 육아블로그 운영하고 있어용
+이웃님 글 통해서 알게 되어 서이추 신청하고 갑니당!
+답방은 꼭 가요 함께 소통해요💗`;
 
 const [blog_id, log_no] = post_url.replace('https://blog.naver.com/', '').split('/');
 const sympathy_url = `https://m.blog.naver.com/SympathyHistoryList.naver?blogId=${blog_id}&logNo=${log_no}&categoryId=POST`;
@@ -43,6 +47,8 @@ try {
     // 서로이웃 추가가 가능하면 추가하고 아니면 닫음
     if (buddyRadio) {
         await buddyRadio.click();
+        await nPage.$eval('.textarea_t1', el => el.value = '');
+        await nPage.type('.textarea_t1', requestContent);
         await nPage.click('.btn_ok');
     } else {
         nPage.close();
